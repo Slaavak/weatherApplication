22993c1fcc80b8bed692f0350ae045746900ab1d
Slava Korolevich <ext_vkorolevich@rendcredit.ru>
2022-11-21 12:29:20 +0300

    Initial Commit

---
+++ weatherApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApplication
+//
+//  Created by Slava Korolevich on 21.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
